f0a05e40c182ffc56d4298308c7812e3ab03a254
govindshaji <govindshaji17@gmail.com>
2026-04-06 22:57:33 +0530

    Third question Done

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,3 +1,15 @@
 public class Triangle {
-    //Add your programs here
+    public static void main(String[] args) {
+        
+        // Loop for 3 rows
+        for (int i = 1; i <= 3; i++) {
+            
+            // Number of stars = 2*i - 1
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("* ");
+            }
+            
+            System.out.println(); // move to next line
+        }
+    }
 }
